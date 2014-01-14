e6063a69fde28750b7b1358f0d829c163b0c8818
liuhanzhong <liuhanzhong@xiangrikui.com>
2014-01-14 16:42:13 +0800

    添加日志

--- spec/active_merchant/billing/integrations/alipay/helper_spec.rb
+++ spec/active_merchant/billing/integrations/alipay/helper_spec.rb
@@ -8,19 +8,13 @@ describe ActiveMerchant::Billing::Integrations::Alipay::Helper do
   end
 
   context 'fields' do
-    params = {
-      :total_fee => 0.01,
-      :seller_email => "13544534667",
-      :notify_url => "pay.xiangrikui.com/payment/getaway/notify",
-      :"_input_charset" => 'utf-8',
-      :service =>  "create_direct_pay_by_user",
-      :payment_type => "2",
-      :subject => "test",
-      :body => "",
-      :return_url => "www.baidu.com".to_s
-    }
+    params = {:subject=>"test", :out_trade_no=>"138968323980539", :total_fee=>"0.01", :payment_type=>"2", :seller_email=>"pay@xiangrikui.com", 
+      :seller_account_name=>"test@xiangrikui.com", :_input_charset=>"utf-8", 
+      :service=>"create_direct_pay_by_user", :partner=>"2088201962733496",
+      :return_url=>nil, :call_back_url=>"www.baidu.com", :merchant_url=>"www.404.com", :pay_expire=>nil, :notify_url=>"http://pay.xiangrikui.com:3000/payment/transaction/alipay/notify"}
+   
     a = ActiveMerchant::Billing::Integrations::Alipay::Helper.new("20140113","208812344321",{})
-    a.query_params params
+    ActiveMerchant::Billing::Integrations::Alipay.execute_uri(params,"rjllq1mf24igskb9y8jaeko6pucsc7g912")
     p a.sign
     p a.fields
   end
